91e049778b214dba7ea82f1dfde095d2bd172e37
Maciej Kedra <maciejken@gmail.com>
2022-02-07 20:00:06 +0100

    rename docker compose files

--- build.groovy
+++ build.groovy
@@ -1,40 +1,48 @@
 pipeline {
   agent any
   parameters {
-    booleanParam(name: "PROD_DEPLOY", defaultValue: false)
-    string(name: "APP_VERSION")
     string(name: "ALLOWED_USERS")
   }
   environment {
     ALLOWED_USERS = "${params.ALLOWED_USERS}"
-    APP_VERSION = "${params.APP_VERSION}"
-    PROD_DEPLOY = "${params.PROD_DEPLOY}"
+    APP_VERSION = "${sh(script: 'npm run -s log:version', returnStdout: true).trim()}_${BUILD_NUMBER}"
   }
   stages {
-    stage('deploy QA') {
-      when {
-        expression { PROD_DEPLOY == 'false' }
+    stage('install') {
+      steps {
+        sh "npm install"
       }
+    }
+    stage('test') {
+      steps {
+        sh "npm test"
+      }
+    }
+    stage('build image') {
+      steps {
+        sh "docker build . -t moje_wydatki_api:${APP_VERSION}"
+      }
+    }
+    stage('deploy QA') {
+      when { branch 'develop' }
       environment {
         DB_USER = credentials('postgres-db-user-qa')
         DB_PASSWORD = credentials('postgres-db-password-qa')
         DB_PATH = credentials('postgres-db-path-qa')
       }
       steps {
-        sh 'docker-compose -f docker_compose/deploy-qa.yml up -d'
+        sh 'docker-compose -f compose.qa-build.yml up -d'
       }
     }
     stage('deploy PROD') {
-      when {
-        expression { PROD_DEPLOY == 'true' }
-      }
+      when { branch 'main' }
       environment {
         DB_USER = credentials('postgres-db-user')
         DB_PASSWORD = credentials('postgres-db-password')
         DB_PATH = credentials('postgres-db-path')
       }
       steps {
-        sh 'docker-compose -f docker_compose/deploy-prod.yml up -d'
+        sh 'docker-compose -f compose.prod-build.yml up -d'
       }
     }
   }
--- deploy.groovy
+++ deploy.groovy
@@ -1,48 +1,40 @@
 pipeline {
   agent any
   parameters {
+    booleanParam(name: "PROD_DEPLOY", defaultValue: false)
+    string(name: "APP_VERSION")
     string(name: "ALLOWED_USERS")
   }
   environment {
     ALLOWED_USERS = "${params.ALLOWED_USERS}"
-    APP_VERSION = "${sh(script: 'npm run -s log:version', returnStdout: true).trim()}_${BUILD_NUMBER}"
+    APP_VERSION = "${params.APP_VERSION}"
+    PROD_DEPLOY = "${params.PROD_DEPLOY}"
   }
   stages {
-    stage('install') {
-      steps {
-        sh "npm install"
+    stage('QA deploy') {
+      when {
+        expression { PROD_DEPLOY == 'false' }
       }
-    }
-    stage('test') {
-      steps {
-        sh "npm test"
-      }
-    }
-    stage('build image') {
-      steps {
-        sh "docker build . -t moje_wydatki_api:${APP_VERSION}"
-      }
-    }
-    stage('deploy QA') {
-      when { branch 'develop' }
       environment {
         DB_USER = credentials('postgres-db-user-qa')
         DB_PASSWORD = credentials('postgres-db-password-qa')
         DB_PATH = credentials('postgres-db-path-qa')
       }
       steps {
-        sh 'docker-compose -f docker_compose/build-qa.yml up -d'
+        sh 'docker-compose -f compose.qa-deploy.yml up -d'
       }
     }
-    stage('deploy PROD') {
-      when { branch 'main' }
+    stage('PROD deploy') {
+      when {
+        expression { PROD_DEPLOY == 'true' }
+      }
       environment {
         DB_USER = credentials('postgres-db-user')
         DB_PASSWORD = credentials('postgres-db-password')
         DB_PATH = credentials('postgres-db-path')
       }
       steps {
-        sh 'docker-compose -f docker_compose/build-prod.yml up -d'
+        sh 'docker-compose -f compose.prod-deploy.yml up -d'
       }
     }
   }
